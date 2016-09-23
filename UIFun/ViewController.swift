978337b5f9e4db6fe325d4b0cb42db4acc24a54c
aacohen <arielaades@gmail.com>
2016-09-22 21:22:50 -0400

    Swift UILab02 0-0 not working

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +44,25 @@ class ViewController: UIViewController {
         
         // TODO: Implement this function according to the instructions.
         
-        
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
+        print(firstSelection)
+        print(secondSelection)
+        switch (firstSelection, secondSelection) {
+        case (0,0):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        case (0,1), (1,0):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
+        case (1,1):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
+        case (0,2), (2,0):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+        case (2,2):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+        default:
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+            
+        }
     }
 
 }
